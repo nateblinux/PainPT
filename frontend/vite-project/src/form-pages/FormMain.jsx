c65acf8237e71d2a988b857f0996405e3f672a1f
Xt6Sub <127432333+Xt6Sub@users.noreply.github.com>
2023-12-01 19:03:37 -0500

    all selection works resizing works css is being worked on need to grab tag on react app href

--- frontend/vite-project/src/form-pages/FormMain.jsx
+++ frontend/vite-project/src/form-pages/FormMain.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
 import FirstPage from './FirstPage';
-import SecondPage from './SecondPage';
+import VerifyPage from './VerifyPage';
 import ResPage from './ResPage';
 import PainScaleActive from './PainScaleActive';
-
+import PainProgression from './PainProgression';
+import SymptomsPage from './Symptoms';
 
 
 export default class FormMain extends Component{
     state = { //store the state of the form - including any response from server
         step: 0,
         input0: '',
-        input1: '',
-        radio: "0",
-        radio1: "0",
-        message: '',
-        location: '',
+        radio_pain_scale_active: "0",
+        radio_pain_scale_rest: "0",
+        pain_progression:'',
+        pain_start_date:'',
+        pain_type: '',
+        pain_experience: '',
+        symptoms_1: '',
+        symptoms_2: '',
+        symptoms_3: '',
+        symptoms_4: '',
+        symptoms_5: '',
+        symptoms_6: '',
+        symptoms_7: '',
+        symptoms_8: '',
+        message: '', //keep message at bottom
     }
 
     //step through form forward + backwards
@@ -25,9 +36,14 @@ export default class FormMain extends Component{
     }
 
     nextStep = () =>{
-        const {step, input0} = this.state;
-        console.log(this.state);
+        const {step} = this.state;
+        //console.log(this.state);
         this.setState({step: step + 1});
+    }
+
+    goToPage = (page) =>{
+        const { step } = this.state;
+        this.setState({step: page});
     }
     
     //handle the changes
@@ -38,6 +54,7 @@ export default class FormMain extends Component{
     //pass to server and get response
     submitForm = async () =>{
         const state = this.state;
+        console.log(state);
         //console.log(input0);
         fetch("http://localhost:8000/", {
             method: "POST",
@@ -73,15 +90,6 @@ export default class FormMain extends Component{
                 )
             case 1:
                 return(
-                    <SecondPage 
-                        nextStep={this.nextStep}
-                        prevStep={this.prevStep}
-                        handleChange={this.handleChange}
-                        submitForm={this.submitForm}
-                        values={values}
-                    />)
-            case 2:
-                return(
                     <PainScaleActive
                         nextStep={this.nextStep}
                         prevStep={this.prevStep}
@@ -90,7 +98,37 @@ export default class FormMain extends Component{
                         values={values}
                     />
                 )
+            case 2:
+                return(
+                    <PainProgression
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+                        handleChange={this.handleChange}
+                        submitForm={this.submitForm}
+                        values={values}
+                    />
+                )
 
+            case 3:
+                return(
+                    <SymptomsPage
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+                        handleChange={this.handleChange}
+                        submitForm={this.submitForm}
+                        values={values}
+                    />
+                )
+
+            case 4: //keep this page last
+                return(
+                    <VerifyPage 
+                        nextStep={this.nextStep}
+                        prevStep={this.prevStep}
+                        handleChange={this.handleChange}
+                        submitForm={this.submitForm}
+                        values={values}
+                    />)
 
             default:
                 return(<ResPage 
