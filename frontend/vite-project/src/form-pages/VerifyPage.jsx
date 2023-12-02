92396ab63c344d50752af5cb69c6885e6d49dbbd
Xt6Sub <127432333+Xt6Sub@users.noreply.github.com>
2023-12-01 19:04:13 -0500

    all selection works resizing works css is being worked on need to grab tag on react app href

---
+++ frontend/vite-project/src/form-pages/VerifyPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const VerifyPage = ({nextStep, prevStep, handleChange, submitForm, values})=>{
+
+    const Continue = e =>{
+        e.preventDefault();
+        submitForm();
+        nextStep();
+    }
+
+    const goBack = e =>{
+        e.preventDefault();
+        prevStep();
+    }
+
+    return(
+        <form action="">
+          <h1>ready to submit?</h1>        
+        <button type="submit" onClick={ Continue }>Continue</button>
+        <button type="submit" onClick={ goBack }>Back</button>
+      </form>
+    )
+}
+
+export default VerifyPage
