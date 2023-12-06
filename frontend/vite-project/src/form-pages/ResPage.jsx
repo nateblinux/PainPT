3c8a70dee9eec8046d721551315996b29836fc11
nateblinux <nateblinux@gmail.com>
2023-12-06 15:33:37 -0500

    added loading spinner

--- frontend/vite-project/src/form-pages/ResPage.jsx
+++ frontend/vite-project/src/form-pages/ResPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import loading from "../assets/loading-band-aid.png"
 
 const ResPage = ({response, prevStep}) =>{
     const goBack = e =>{
@@ -6,10 +7,24 @@ const ResPage = ({response, prevStep}) =>{
         prevStep();
     }
 
+    const processRes = res =>{
+        if(res){
+            return (
+                <pre>{res}</pre>
+            )
+        }else{
+            return (
+                <div className="loader">
+                    <img width={75} src = {loading}></img>
+                </div>
+            )
+        }
+    }
+
     return(
         <>
         <form>
-        <pre> {response} </pre>
+            {processRes(response)}
             <button type="submit" onClick={ goBack }>Back</button>
         </form>
         </>
